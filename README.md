644fffe2766439433b62e1ef391325b849c81a65
Eric Trotchie <ejtrotchie@gmail.com>
2017-01-21 14:46:08 -0700

    initial commit - basic structure and style

---
+++ README.md
@@ -0,0 +1,39 @@
+My starter kit - WORK IN PROGGRESS
+=============================
+
+## Description
+
+A simple kit to get you started. Don't worry about a damn thing, let [Gulp](http://gulpjs.com/) take care of the build process.
+
+
+## This project uses the following tools
+
+[Gulp](http://gulpjs.com/)
+[Pug](https://github.com/pugjs/pug)
+[Babel](https://babeljs.io/)
+[ESLINT](http://eslint.org/)
+[SASS](http://sass-lang.com/)
+[Autoprefix](https://autoprefixer.github.io/)
+[BrowserSync](https://browsersync.io/)
+
+## System Preparation
+
+To use this project, you'll need the following things installed on your machine.
+
+1. [NodeJS](http://nodejs.org) - use the installer.
+2. [GulpJS](https://github.com/gulpjs/gulp) - `$ npm install -g gulp` (mac users may need sudo)
+
+## Local Installation
+
+1. Clone or fork this repo, or download it into a directory of your choice.
+2. Inside the directory, run `npm install`.
+
+## Usage
+
+**development mode**
+
+Jus type gulp into the terminal of the root of your project. A browser will open and you can view your project while you work and watch the changes happen as you edit and save you files.
+
+```shell
+$ gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const GULP         = require("gulp");
+const BABEL        = require("gulp-babel");
+const ESLINT       = require("gulp-eslint");
+const PUG          = require("gulp-pug");
+const SASS         = require("gulp-sass");
+const PREFIX       = require("gulp-autoprefixer");
+const BROWSER_SYNC = require("browser-sync");
+
+/*
+* Run ESLINT on your ecma2015 code
+*/
+GULP.task("eslint", function() {
+  GULP.src("2-assets/js/*.js")
+  .pipe(ESLINT())
+  .pipe(ESLINT.format());
+});
+
+/*
+*  Build you pug files
+*/
+GULP.task("pug", function() {
+  return GULP.src("index.pug")
+  .pipe(PUG())
+  .pipe(GULP.dest("_site/"))
+  .pipe(BROWSER_SYNC.reload({stream:true}));
+});
+
+/*
+* Babel - compile your ecma2015 JavaScript
+*/
+GULP.task("babel", function() {
+  // Browser source
+  return GULP.src("2-assets/js/main.js")
+  .pipe(BABEL())
+  .pipe(GULP.dest("_site/assets/js"))
+  .pipe(BROWSER_SYNC.reload({stream:true}));
+});
+
+/**
+ * Compile scss files
+ */
+GULP.task("sass", function () {
+  return GULP.src("2-assets/css/main.scss")
+  .pipe(SASS({
+    includePaths: ["css"],
+    onError: BROWSER_SYNC.notify
+  }))
+  .pipe(PREFIX(["last 15 versions", "> 1%", "ie 8", "ie 7"], { cascade: true }))
+  .pipe(GULP.dest("_site/assets/css"))
+  .pipe(BROWSER_SYNC.reload({stream:true}));
+});
+
+/*
+* Run browser-sync
+*/
+GULP.task("browser-sync",["sass", "eslint", "babel", "pug"], function() {
+  BROWSER_SYNC({
+    server: {
+      baseDir: "_site"
+    },
+    notify: false
+  });
+});
+
+/*
+* Watch folders for changes
+*/
+GULP.task("watch", function() {
+  GULP.watch("2-assets/js/**", ["eslint", "babel"]);
+  GULP.watch("2-assets/css/**", ["sass"]);
+  GULP.watch("index.pug", ["pug"]);
+  GULP.watch("1-includes/*", ["pug"]);
+  GULP.watch("1-includes/*/**", ["pug"]);
+});
+
+GULP.task("default", ["browser-sync", "watch"]);
